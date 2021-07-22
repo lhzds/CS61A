0e435969957530780dadb8cf828cd63cf438426c
lhzds <pentahan8@gmail.com>
2021-07-22 19:15:22 +0800

    labs, hws and projects

---
+++ lab/lab08/lab08.py
@@ -0,0 +1,50 @@
+def make_generators_generator(g):
+    """Generates all the "sub"-generators of the generator returned by
+    the generator function g.
+
+    >>> def every_m_ints_to(n, m):
+    ...     i = 0
+    ...     while (i <= n):
+    ...         yield i
+    ...         i += m
+    ...
+    >>> def every_3_ints_to_10():
+    ...     for item in every_m_ints_to(10, 3):
+    ...         yield item
+    ...
+    >>> for gen in make_generators_generator(every_3_ints_to_10):
+    ...     print("Next Generator:")
+    ...     for item in gen:
+    ...         print(item)
+    ...
+    Next Generator:
+    0
+    Next Generator:
+    0
+    3
+    Next Generator:
+    0
+    3
+    6
+    Next Generator:
+    0
+    3
+    6
+    9
+    """
+    "*** YOUR CODE HERE ***"
+    count_e = 0
+    for e in g():
+        count_e += 1
+
+        def sub_generator():
+            count_sub_e = 0
+            for sub_e in g():
+                if count_sub_e >= count_e:
+                    break
+                yield sub_e
+                count_sub_e += 1
+
+        yield sub_generator()
+
+
